83e763217a3918a718b5b2d64a5457909ff04e05
Arthur Rio <arthur.rio44@gmail.com>
2021-05-25 18:31:01 -0600

    Fix bandit

--- bandit/test.py
+++ bandit/test.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 def test(number: int) -> str:
-    access_token = "1234"
-    password = "7890"
+    access_token = "1234"  # nosec: B105
+    password = "7890"  # nosec: B105
     print(password)
+    # comment
